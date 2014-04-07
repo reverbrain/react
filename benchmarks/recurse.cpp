29fe19fd235c90fffdf78fbc4810b045aac5fcda
acid <acid@yandex-team.ru>
2014-04-07 15:54:58 +0400

    benchmark: Simple benchmarks for deep and flat tree added

---
+++ benchmarks/recurse.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#include "react/react.h"
+
+size_t MAX_DEPTH = 1000000;
+size_t x = 2;
+int action_code = react_define_new_action("ACTION");
+
+void recurse(size_t depth) {
+	if (depth > MAX_DEPTH) {
+		return;
+	}
+	react_start_action(action_code);
+	x *= x;
+	recurse(depth + 1);
+	react_stop_action(action_code);
+}
+
+int main() {
+
+	size_t start_time = clock();
+
+	react_context_t *context = react_activate();
+	recurse(0);
+	react_deactivate(context);
+
+	std::cerr << x << std::endl;
+	size_t stop_time = clock();
+
+	double total_time = double(stop_time - start_time) / CLOCKS_PER_SEC * 1000;
+	std::cerr << "Total time: " << total_time << "ms" << std::endl;
+	double time_per_operation = total_time / MAX_DEPTH;
+	std::cerr << "Time per operation: " << time_per_operation << "ms" << std::endl;
+	int operations_per_sec = 1000 / time_per_operation;
+	std::cerr << "Operations per sec: " << operations_per_sec << std::endl;
+
+	return 0;
+}
+
