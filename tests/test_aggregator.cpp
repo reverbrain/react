5dae2d0a2a002c024b5afed89f81ed2400ddc5af
acid <acid@yandex-team.ru>
2014-04-21 15:28:54 +0400

    tests: simple aggregator test added

---
+++ tests/test_aggregator.cpp
@@ -0,0 +1,20 @@
+#include <stdexcept>
+
+#include "tests.hpp"
+
+#include "react/aggregator.hpp"
+
+BOOST_AUTO_TEST_SUITE( aggregator_suite )
+
+using namespace react;
+
+BOOST_AUTO_TEST_CASE( recent_trees_aggregator_test )
+{
+	recent_trees_aggregator_t aggregator(2);
+	actions_set_t actions_set;
+	call_tree_t call_tree(actions_set);
+	aggregator.aggregate(call_tree);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
