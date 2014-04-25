51573af5c2e6a933c160481e12be2d1923bc6928
acid <acid@yandex-team.ru>
2014-04-25 19:40:08 +0400

    tests: histogram tests added. fix in aggregators constructors

--- tests/test_aggregator.cpp
+++ tests/test_aggregator.cpp
@@ -4,6 +4,8 @@
 
 #include "react/aggregators/recent_trees_aggregator.hpp"
 #include "react/aggregators/unordered_call_tree_aggregator.hpp"
+#include "react/aggregators/histogram_aggregator.hpp"
+#include "react/utils.hpp"
 
 BOOST_AUTO_TEST_SUITE( aggregator_suite )
 
@@ -11,8 +13,8 @@ using namespace react;
 
 BOOST_AUTO_TEST_CASE( recent_trees_aggregator_test )
 {
-	recent_trees_aggregator_t aggregator(2);
 	actions_set_t actions_set;
+	recent_trees_aggregator_t aggregator(actions_set, 2);
 	call_tree_t call_tree(actions_set);
 	aggregator.aggregate(call_tree);
 }
@@ -25,5 +27,22 @@ BOOST_AUTO_TEST_CASE( unordered_call_tree_aggregator_test )
 	aggregator.aggregate(call_tree);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_CASE( histogram1D_test )
+{
+	std::vector<int> ticks = {1, 2, 4, 8, 16};
+	histogram1D_t histogram1D(ticks);
+	histogram1D.update(0);
+	histogram1D.update(1);
 
+	BOOST_REQUIRE_EQUAL( histogram1D.get(0), 1 );
+}
+
+BOOST_AUTO_TEST_CASE( histogram2D_test )
+{
+	std::vector<int> ticks = {1, 2, 4, 8, 16};
+	histogram2D_t histogram2D(ticks, ticks);
+	histogram2D.update(0, 3);
+	histogram2D.update(1, 3);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
