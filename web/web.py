86d1db43d1d50ea141e38972e07115b4af036c70
acid <acid@yandex-team.ru>
2014-04-28 22:38:56 +0400

    web: web interface initial commit

---
+++ web/web.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html", host='"localhost:20000"')
+
+
+if __name__ == '__main__':
+    app.run()
