d3cb0a5ff4b3972ec482be973341671bc955cb99
acid <acid@yandex-team.ru>
2014-04-28 11:41:03 +0400

    aggregators: call_tree replaced with concurrent_call_tree to avoid races

--- include/react/aggregators/parent_call_tree_aggregator.hpp
+++ include/react/aggregators/parent_call_tree_aggregator.hpp
@@ -23,7 +23,7 @@ namespace react {
 class parent_call_tree_aggregator_t : public aggregator_t {
 public:
 	parent_call_tree_aggregator_t(const actions_set_t &actions_set,
-								  call_tree_t &parent_call_tree,
+								  concurrent_call_tree_t &parent_call_tree,
 								  call_tree_t::p_node_t parent_node):
 		aggregator_t(actions_set),
 		parent_call_tree(parent_call_tree),
@@ -31,19 +31,23 @@ public:
 	~parent_call_tree_aggregator_t() {}
 
 	void aggregate(const call_tree_t &call_tree) {
-		call_tree.merge_into(parent_node, parent_call_tree);
+		std::lock_guard<concurrent_call_tree_t> guard(parent_call_tree);
+		call_tree.merge_into(parent_node, parent_call_tree.get_call_tree());
 	}
 
 	void to_json(rapidjson::Value &value, rapidjson::Document::AllocatorType &allocator) const {
 		rapidjson::Value aggregator_value(rapidjson::kObjectType);
-		int action_code = parent_call_tree.get_node_action_code(parent_node);
-		std::string action_name = parent_call_tree.get_actions_set().get_action_name(action_code);
+		auto call_tree = parent_call_tree.copy_call_tree();
+		int action_code = call_tree.get_node_action_code(parent_node);
+		rapidjson::Value action_name(
+			call_tree.get_actions_set().get_action_name(action_code).c_str(), allocator
+		);
 		aggregator_value.AddMember("parent_action", action_name, allocator);
 		value.AddMember("parent_call_tree_aggregator", aggregator_value, allocator);
 	}
 
 private:
-	call_tree_t &parent_call_tree;
+	concurrent_call_tree_t &parent_call_tree;
 	call_tree_t::p_node_t parent_node;
 };
 
