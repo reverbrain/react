7583cd1900cd548947c059dbd6e23545685b36d6
acid <acid@yandex-team.ru>
2014-05-06 20:16:37 +0400

    api: Docs added.

--- include/react/react.hpp
+++ include/react/react.hpp
@@ -26,31 +26,71 @@
 
 namespace react {
 
+/*!
+ * \brief Wrapper for action_guard_t with binded updater from local context
+ */
 class action_guard {
 public:
+	/*!
+	 * \brief Creates action_guard and starts action with \a action_code
+	 * \param Code of started action
+	 */
 	explicit action_guard(int action_code);
+
 	action_guard(const action_guard &other) = delete;
+
+	/*!
+	 * \brief Stops guarded action.
+	 */
 	~action_guard();
 
 	action_guard &operator =(const action_guard &other) = delete;
 
+	/*!
+	 * \brief Allows to stop action manually
+	 */
 	void stop();
 
 private:
+	/*!
+	 * \brief Wrapped action_guard_t
+	 */
 	std::unique_ptr<react::action_guard_t> m_action_guard;
 };
 
+/*!
+ * \brief Returns current context actions set
+ * \return Current context action set
+ */
 const actions_set_t &get_actions_set();
 
+/*!
+ * \internal
+ *
+ * \brief Adds new stat to current call tree
+ * \param key Name of stat
+ * \param value Value of stat
+ */
 void add_stat_impl(const std::string &key, const react::stat_value_t &value);
 
+/*!
+ * \brief Template wrapper for add_stat_impl. Avoids specifying value type manually.
+ */
 template<typename T>
 void add_stat(const std::string &key, const T &value) {
 	add_stat_impl(key, react::stat_value_t(value));
 }
 
+/*!
+ * \brief Template specification for add_stat that handles char* correctly.
+ */
 void add_stat(const std::string &key, const char *value);
 
+/*!
+ * \brief Creates aggregator that can be passed to subthread in order to monitor it
+ *          and merge result of monitoring with current thread context
+ * \return Returns shared_ptr to newly created aggregator for subthread
+ */
 std::shared_ptr<aggregator_t> create_subthread_aggregator();
 
 } // namespace react
